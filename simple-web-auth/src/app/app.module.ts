b1a6a7991279374207fae2ebe6e159a43224d57f
Jany Elias <31951013+EliasJany@users.noreply.github.com>
2021-03-11 12:08:43 +0100

    Update app.module.ts

--- simple-web-auth/src/app/app.module.ts
+++ simple-web-auth/src/app/app.module.ts
@@ -1,16 +1,37 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MsalInterceptor, MsalModule, MsalService, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
 
 import { AppComponent } from './app.component';
+import { MSALInstanceFactory, MSALInterceptorConfigFactory } from './msal';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MsalModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: MSAL_INSTANCE,
+    useFactory: MSALInstanceFactory,
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: MsalInterceptor,
+    multi: true,
+  },
+  {
+    provide: MSAL_INTERCEPTOR_CONFIG,
+    useFactory: MSALInterceptorConfigFactory,
+  },
+  MsalService,
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
